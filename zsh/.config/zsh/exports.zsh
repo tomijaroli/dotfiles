3d8a43542955826eaaea37d077ff4358b2772556
Tomi Jaroli <tamas.jaroli@wolt.com>
2023-07-20 11:59:25 +0300

    Add android tools path to exports

--- zsh/.config/zsh/exports.zsh
+++ zsh/.config/zsh/exports.zsh
@@ -33,5 +33,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
 
+export ANDROID_HOME=/Users/$USER/Library/Android/sdk
+export PATH="$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools"
+
 eval "$(zoxide init zsh)"
 eval "$(dircolors ~/.nix-profile/share/LS_COLORS)"
