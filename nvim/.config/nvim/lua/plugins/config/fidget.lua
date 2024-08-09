1ce5577a55576135a5f142335f5122383cb0847a
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 19:40:08 +0300

    [vim] Extract fidget config

---
+++ nvim/.config/nvim/lua/plugins/config/fidget.lua
@@ -0,0 +1,18 @@
+local fidget = require "fidget"
+
+fidget.setup {
+    notification = {
+        window = {
+            normal_hl = "String",
+            winblend = 0,
+            border = "rounded",
+            zindex = 45,
+            max_width = 0,
+            max_height = 0,
+            x_padding = 1,
+            y_padding = 1,
+            align = "bottom",
+            relative = "editor",
+        },
+    },
+}
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -7,22 +7,10 @@ return {
         {
             "j-hui/fidget.nvim",
             event = "VeryLazy",
-            opts = {
-                notification = {
-                    window = {
-                        normal_hl = "String",
-                        winblend = 0,
-                        border = "rounded",
-                        zindex = 45,
-                        max_width = 0,
-                        max_height = 0,
-                        x_padding = 1,
-                        y_padding = 1,
-                        align = "bottom",
-                        relative = "editor",
-                    },
-                },
-            },
+            opts = {},
+            config = function()
+                require "plugins.config.fidget"
+            end,
         },
     },
     config = function()
