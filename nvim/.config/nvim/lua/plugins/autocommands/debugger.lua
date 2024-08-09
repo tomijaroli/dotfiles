f0790780fe77d1cf80d058bf367bc4b44dfd91e4
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 20:34:10 +0300

    [vim] Extract debugger autocommands

---
+++ nvim/.config/nvim/lua/plugins/autocommands/debugger.lua
@@ -0,0 +1,18 @@
+local group = vim.api.nvim_create_augroup("dapui_config", { clear = true })
+
+-- hide ~ in DAPUI
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = group,
+    pattern = "DAP*",
+    callback = function()
+        vim.wo.fillchars = "eob: "
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = group,
+    pattern = "\\[dap\\-repl\\]",
+    callback = function()
+        vim.wo.fillchars = "eob: "
+    end,
+})
--- nvim/.config/nvim/lua/plugins/config/debuger.lua
+++ nvim/.config/nvim/lua/plugins/config/debuger.lua
@@ -120,23 +120,7 @@ dapui.setup {
 
 vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
 
-local group = vim.api.nvim_create_augroup("dapui_config", { clear = true })
-
--- hide ~ in DAPUI
-vim.api.nvim_create_autocmd("BufWinEnter", {
-    group = group,
-    pattern = "DAP*",
-    callback = function()
-        vim.wo.fillchars = "eob: "
-    end,
-})
-vim.api.nvim_create_autocmd("BufWinEnter", {
-    group = group,
-    pattern = "\\[dap\\-repl\\]",
-    callback = function()
-        vim.wo.fillchars = "eob: "
-    end,
-})
+require "plugins.autocommands.debugger"
 
 dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 dap.listeners.before.event_terminated["dapui_config"] = dapui.close
