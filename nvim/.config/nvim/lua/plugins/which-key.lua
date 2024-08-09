d70a3db6d7ed89bf5a347888d3c9877f1e07d7eb
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 20:14:55 +0300

    [vim] Extract which-key config

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -2,22 +2,6 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     config = function()
-        local which_key = require "which-key"
-        which_key.setup()
-
-        which_key.add {
-            { "<leader>c", group = "[C]ode" },
-            { "<leader>d", group = "[D]ocument" },
-            { "<leader>g", group = "[G]itsign" },
-            { "<leader>d_", hidden = true },
-            { "<leader>r", group = "[R]ename" },
-            { "<leader>r_", hidden = true },
-            { "<leader>s", group = "[S]earch" },
-            { "<leader>s_", hidden = true },
-            { "<leader>t", group = "[T]ab management" },
-            { "<leader>x", group = "[x]codebuild actions" },
-            { "<leader>w", group = "[W]orkspace" },
-            { "<leader>w_", hidden = true },
-        }
+        require "plugins.config.which-key"
     end,
 }
