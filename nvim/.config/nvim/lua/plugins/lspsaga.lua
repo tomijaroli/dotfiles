315c49c64f37e8839d2b8aa7b1239072a2d22f69
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 19:41:15 +0300

    [vim] Extract lspsaga config

--- nvim/.config/nvim/lua/plugins/lspsaga.lua
+++ nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -1,21 +1,10 @@
 return {
     "nvimdev/lspsaga.nvim",
-    config = function()
-        require("lspsaga").setup {
-            move_in_saga = {
-                prev = "<C-k>",
-                next = "<C-j>",
-            },
-            finder_action_keys = {
-                open = "<CR>",
-            },
-            definition_action_keys = {
-                edit = "<CR>",
-            },
-        }
-    end,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+        require "plugins.config.lspsaga"
+    end,
 }
