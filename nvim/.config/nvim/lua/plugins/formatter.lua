2fc6924f5324b8aca83ca025c376e6a157ad3434
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 18:40:11 +0300

    vim - reformat config files

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,30 +1,30 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            notify_on_error = false,
-            format_on_save = function(bufnr)
-                local disable_filetypes = { c = true, cpp = true }
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                }
-            end,
-            log_level = vim.log.levels.ERROR,
-            -- TODO: fix ruby formatting
-            formatters_by_ft = {
-                lua = { "stylua" },
-                go = { "gofumpt", "goimports_reviser", "golines" },
-                javascript = { { "prettierd", "prettier" } },
-                python = { "isort", "black" },
-                ruby = { "rubocop" },
-                swift = { "swiftformat" },
-                yaml = { "yamlls" },
-            },
-        },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      log_level = vim.log.levels.ERROR,
+      -- TODO: fix ruby formatting
+      formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "gofumpt", "goimports_reviser", "golines" },
+        javascript = { { "prettierd", "prettier" } },
+        python = { "isort", "black" },
+        ruby = { "rubocop" },
+        swift = { "swiftformat" },
+        yaml = { "yamlls" },
+      },
     },
-    {
-        "gennaro-tedesco/nvim-jqx",
-        ft = { "json" },
-    },
+  },
+  {
+    "gennaro-tedesco/nvim-jqx",
+    ft = { "json" },
+  },
 }
