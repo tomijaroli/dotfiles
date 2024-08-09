7016392ff5d822cfb3fb476f2536ae38045147c4
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 20:00:10 +0300

    [vim] Extract treesitter config

---
+++ nvim/.config/nvim/lua/plugins/config/nvim-treesitter.lua
@@ -0,0 +1,26 @@
+local treesitter = require "nvim-treesitter.configs"
+
+treesitter.setup {
+    ensure_installed = {
+        "bash",
+        "c",
+        "gitignore",
+        "go",
+        "html",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "swift",
+        "vim",
+        "vimdoc",
+        "yaml",
+    },
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { "ruby" },
+    },
+    indent = { enable = true, disable = { "ruby" } },
+    autotag = { enable = true },
+}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = {
-        ensure_installed = {
-            "bash",
-            "c",
-            "gitignore",
-            "go",
-            "html",
-            "json",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "swift",
-            "vim",
-            "vimdoc",
-            "yaml",
-        },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { "ruby" },
-        },
-        indent = { enable = true, disable = { "ruby" } },
-        autotag = { enable = true },
-    },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+    opts = {},
+    config = function()
+        require "plugins.config.nvim-treesitter"
     end,
 }
