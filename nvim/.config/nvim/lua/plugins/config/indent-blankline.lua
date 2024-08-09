5db5d9cf5783ffb8ef9389e4be0caea7337fc5ed
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 19:05:25 +0300

    [vim] Extract indent blankline config

---
+++ nvim/.config/nvim/lua/plugins/config/indent-blankline.lua
@@ -0,0 +1,20 @@
+local ibl = require "ibl"
+
+ibl.setup {
+    scope = { enabled = false },
+    exclude = {
+        filetypes = {
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "trouble",
+            "lazy",
+            "mason",
+            "notify",
+            "toggleterm",
+            "lazyterm",
+        },
+    },
+}
--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -7,24 +7,10 @@ return {
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         event = "VeryLazy",
-        opts = {
-            scope = { enabled = false },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-            },
-        },
+        opts = {},
+        config = function()
+            require "plugins.config.indent-blankline"
+        end,
     },
     {
         "windwp/nvim-autopairs",
