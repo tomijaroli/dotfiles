479e68f73816b27ab2267d3a55779cb6d17568ea
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 19:19:08 +0300

    [vim] Add missing dependency to lazygit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -8,6 +8,9 @@ return {
     },
     {
         "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
         cmd = {
             "LazyGit",
             "LazyGitConfig",
