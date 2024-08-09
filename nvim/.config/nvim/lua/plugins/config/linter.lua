377bf77300b11ec31dcf0f6ac495729a3b5f90f8
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 20:30:15 +0300

    [vim] Extract linter autocommand

--- nvim/.config/nvim/lua/plugins/config/linter.lua
+++ nvim/.config/nvim/lua/plugins/config/linter.lua
@@ -1,18 +1,9 @@
-local lint = require "lint"
-
-lint.linters_by_ft = {
+require("lint").linters_by_ft = {
     swift = { "swiftlint" },
     markdown = { "markdownlint" },
 }
 
-local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
-    group = lint_augroup,
-    callback = function()
-        require("lint").try_lint()
-    end,
-})
+require "plugins.autocommands.linter"
 
 vim.keymap.set("n", "<leader>ml", function()
     require("lint").try_lint()
