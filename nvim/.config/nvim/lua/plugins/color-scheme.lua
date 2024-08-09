4e1aa2a3cc7419d49de0194421bf764ee7adbc23
Tomi Jaroli <tamas.jaroli@gmail.com>
2024-08-09 18:51:17 +0300

    [vim] Extract color scheme config - nord

--- nvim/.config/nvim/lua/plugins/color-scheme.lua
+++ nvim/.config/nvim/lua/plugins/color-scheme.lua
@@ -1,59 +1,11 @@
-local styles
-
-if vim.g.user_enable_transparent_background then
-  styles = {
-    sidebars = "transparent",
-    floats = "transparent",
-  }
-end
-
 return {
   "gbprod/nord.nvim",
   name = "nord",
   priority = 1000,
   lazy = false,
-  opts = {
-    transparent_background = vim.g.user_enable_transparent_background,
-    styles = styles,
-    integrations = {
-      cmp = true,
-      gitsigns = true,
-      indent_blankline = {
-        enabled = false,
-        scope_color = "sapphire",
-        colored_indent_levels = false,
-      },
-      mason = true,
-      native_lsp = { enabled = true },
-      notify = true,
-      neotree = true,
-      symbols_outline = true,
-      telescope = true,
-      treesitter = true,
-      treesitter_context = true,
-    },
-    dim_inactive = {
-      enabled = false,
-      shade = "light",
-      percentage = 0.9,
-    },
-    custom_highlights = function(colors)
-      local searchActive = { bg = colors.red, fg = "#181825" }
-      local searchInactive = { bg = colors.peach, fg = "#000000" }
-
-      return {
-        CursorLineNr = { fg = colors.flamingo },
-        Search = searchInactive,
-        IncSearch = searchActive,
-        EndOfBuffer = { fg = colors.flamingo },
-        WinSeparator = { fg = colors.surface0, bg = colors.surface0 },
-      }
-    end,
-  },
-  config = function(_, opts)
-    require("nord").setup(opts)
-    vim.cmd.colorscheme "nord"
-    vim.cmd.hi "Comment gui=none"
+  opts = {},
+  config = function()
+    require "plugins.config.nord"
   end,
 }
 
---
+++ nvim/.config/nvim/lua/plugins/config/nord.lua
@@ -0,0 +1,50 @@
+local styles
+
+if vim.g.user_enable_transparent_background then
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  }
+end
+
+require("nord").setup {
+  transparent_background = vim.g.user_enable_transparent_background,
+  styles = styles,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    indent_blankline = {
+      enabled = false,
+      scope_color = "sapphire",
+      colored_indent_levels = false,
+    },
+    mason = true,
+    native_lsp = { enabled = true },
+    notify = true,
+    neotree = true,
+    symbols_outline = true,
+    telescope = true,
+    treesitter = true,
+    treesitter_context = true,
+  },
+  dim_inactive = {
+    enabled = false,
+    shade = "light",
+    percentage = 0.9,
+  },
+  custom_highlights = function(colors)
+    local searchActive = { bg = colors.red, fg = "#181825" }
+    local searchInactive = { bg = colors.peach, fg = "#000000" }
+
+    return {
+      CursorLineNr = { fg = colors.flamingo },
+      Search = searchInactive,
+      IncSearch = searchActive,
+      EndOfBuffer = { fg = colors.flamingo },
+      WinSeparator = { fg = colors.surface0, bg = colors.surface0 },
+    }
+  end,
+}
+
+vim.cmd.colorscheme "nord"
+vim.cmd.hi "Comment gui=none"
