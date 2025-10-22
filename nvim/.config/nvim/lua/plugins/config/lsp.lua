a85c7e2f7475311f637f47a65f9faa4c3191c97e
Tomi Jaroli <hi@tomijaroli.me>
2025-10-22 19:59:06 +0300

    [nvim] Fix compatibility issues after update

--- nvim/.config/nvim/lua/plugins/config/lsp.lua
+++ nvim/.config/nvim/lua/plugins/config/lsp.lua
@@ -4,14 +4,19 @@ local lsp_autocommand_callbacks = require "plugins.autocommands.callbacks.lsp"
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-local util = require "lspconfig.util"
-
--- Change the Diagnostic symbols in the sign column (gutter)
-local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
+vim.diagnostic.config {
+  signs = {
+    severity = {
+      min = vim.diagnostic.severity.HINT,
+    },
+    values = {
+      Error = "",
+      Warn = "",
+      Hint = "󰠠",
+      Info = "",
+    },
+  },
+}
 
 local servers = {
   clangd = {},
@@ -20,12 +25,8 @@ local servers = {
   lua_ls = {
     settings = {
       Lua = {
-        completion = {
-          callSnippet = "Replace",
-        },
-        diagnostics = {
-          globals = { "vim" },
-        },
+        completion = { callSnippet = "Replace" },
+        diagnostics = { globals = { "vim" } },
       },
     },
   },
@@ -35,26 +36,31 @@ local servers = {
 }
 
 require("mason").setup()
-
-local ensure_installed = vim.tbl_keys(servers or {})
-vim.list_extend(ensure_installed, {
-  "stylua",
-})
+local ensure_installed = vim.tbl_keys(servers)
+vim.list_extend(ensure_installed, { "stylua" })
 require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
-local lspconfig = require "lspconfig"
+local function start_server(name, opts)
+  vim.lsp.start(vim.tbl_deep_extend("force", {
+    name = name,
+    cmd = { name },
+    capabilities = capabilities,
+    on_attach = lsp_autocommand_callbacks.lsp_on_attach,
+    root_dir = vim.fs.dirname(
+      vim.fs.find({ ".git", "package.json", "setup.py", "Makefile", "tsconfig.json" }, { upward = true })[1]
+    ),
+  }, opts or {}))
+end
+
 require("mason-lspconfig").setup {
   handlers = {
     function(server_name)
-      local server = servers[server_name] or {}
-      server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-      lspconfig[server_name].setup(server)
+      start_server(server_name, servers[server_name])
     end,
   },
 }
 
-lspconfig["sourcekit"].setup {
-  capabilities = capabilities,
-  on_attach = lsp_autocommand_callbacks.lsp_on_attach,
-  root_dir = lspconfig.util.root_pattern(".git", "Package.swift", "compile_commands.json"),
-}
+start_server("sourcekit", {
+  cmd = { "sourcekit-lsp" },
+  root_dir = vim.fs.dirname(vim.fs.find({ ".git", "Package.swift", "compile_commands.json" }, { upward = true })[1]),
+})
