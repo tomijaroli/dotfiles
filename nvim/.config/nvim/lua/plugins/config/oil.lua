3ffd06a3f0d40edfe8a3744fd23e26bdec2aee6a
Tomi Jaroli <hi@tomijaroli.me>
2025-11-04 20:13:22 +0200

    [nvim] Inject dependencies into Oil

--- nvim/.config/nvim/lua/plugins/config/oil.lua
+++ nvim/.config/nvim/lua/plugins/config/oil.lua
@@ -1,23 +1,19 @@
-M = {}
-
-M.repository = 'stevearc/oil.nvim'
----@module 'oil'
----@type oil.SetupOpts
-M.opts = {
-  view_options = {
-	show_hidden = true,
-  }
-}
-M.keys = {
-  {
-    "-",
-    function() require("oil").open_float() end,
-    desc = "Open Oil (floating)",
-    mode = "n",
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    view_options = {
+      show_hidden = true,
+    }
   },
+  keys = {
+    {
+      "-",
+      function() require("oil").open_float() end,
+      desc = "Open Oil (floating)",
+      mode = "n",
+    },
+  },
+  lazy = false
 }
-M.dependencies = { "nvim-tree/nvim-web-devicons" }
--- M.dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-M.lazy = false
-
-return M
--- nvim/.config/nvim/lua/plugins/directory-traversing.lua
+++ nvim/.config/nvim/lua/plugins/directory-traversing.lua
@@ -1,9 +1,14 @@
-local config = require "plugins.config.oil"
+local di = require("plugins.utils.dependency-injection")
+local dependencies_registry = require("plugins.dependencies.registry")
+
+local dependencies = {
+  dependencies_registry.keys.web_devicons,
+}
+
+local oil = require 'plugins.config.oil'
+
+di.inject_dependencies_by_key(oil, dependencies, dependencies_registry)
 
 return {
-  config.repository,
-  opts = config.opts,
-  keys = config.keys,
-  dependencies = config.dependencies,
-  lazy = config.lazy,
+  oil
 }
