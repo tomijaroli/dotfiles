278068b841d4796528976f068697a270490701fa
Tomi Jaroli <hi@tomijaroli.me>
2025-11-04 17:55:48 +0200

    [nvim] Editor config

---
+++ nvim/.config/nvim/lua/plugins/config/editor/todo-comments.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/todo-comments.nvim",
+  event = "BufReadPost",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    signs = false,
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,7 @@
+return {
+    require 'plugins.config.editor.autopairs',
+    require 'plugins.config.editor.indent-blankline',
+    require 'plugins.config.editor.nvim-colorizer',
+    require 'plugins.config.editor.sleuth',
+    require 'plugins.config.editor.todo-comments',
+}
