e841fa27d8642114cc0841ead37b10abf96348a5
Tomi Jaroli <hi@tomijaroli.me>
2025-11-04 20:13:49 +0200

    [nvim] Inject dependencies into UI config

--- nvim/.config/nvim/lua/plugins/config/ui/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/config/ui/bufferline.lua
@@ -2,7 +2,6 @@ return {
   "akinsho/bufferline.nvim",
   version = "*",
   event = "VeryLazy",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
       mode = "buffers",
--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,18 @@
+local di = require("plugins.utils.dependency-injection")
+local dependencies_registry = require("plugins.dependencies.registry")
+
+local dependencies = {
+  dependencies_registry.keys.web_devicons,
+}
+
+local bufferline = require 'plugins.config.ui.bufferline'
+local lualine = require 'plugins.config.ui.lualine'
+
+di.inject_dependencies_by_key(bufferline, dependencies, dependencies_registry)
+di.inject_dependencies_by_key(lualine, dependencies, dependencies_registry)
+
 return {
-    require 'plugins.config.ui.bufferline',
-    require 'plugins.config.ui.lualine',
+    bufferline,
+    lualine,
     require 'plugins.config.ui.nightfox',
 }
