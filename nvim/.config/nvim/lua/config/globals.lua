7d16a806952548b15854f6ffa005903c7a66e938
Tomi Jaroli <hi@tomijaroli.me>
2025-11-04 09:52:09 +0200

    [nvim] Add globals config

--- nvim/.config/nvim/lua/config/globals.lua
+++ nvim/.config/nvim/lua/config/globals.lua
@@ -1,1 +1,19 @@
+-- ===============================
+-- Leader keys
+-- ===============================
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- ===============================
+-- Disable built-in netrw (for using Oil)
+-- ===============================
+vim.g.loaded = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- ===============================
+-- Plugin globals
+-- ===============================
+vim.g.autopairs_enabled = true
+vim.g.have_nerd_font = true
+
