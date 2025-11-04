28b12cacbab794964c7800fb6e0e0509e7bad88a
Tomi Jaroli <hi@tomijaroli.me>
2025-11-04 15:50:34 +0200

    [nvim] Cleanup

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -15,9 +15,7 @@ return {
       mode = "n",
     },
   },
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  -- dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
 }
