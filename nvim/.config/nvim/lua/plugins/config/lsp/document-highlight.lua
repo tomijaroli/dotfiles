0363c8c25f1d66f0f39023802b4d81c0e4406091
Tomi Jaroli <hi@tomijaroli.me>
2025-11-24 14:22:40 +0200

    [nvim] Fix document highlight auto group

--- nvim/.config/nvim/lua/plugins/config/lsp/document-highlight.lua
+++ nvim/.config/nvim/lua/plugins/config/lsp/document-highlight.lua
@@ -40,7 +40,7 @@ M.setup = function(event)
       group = vim.api.nvim_create_augroup("user-lsp-detach", { clear = true }),
       callback = function(event2)
         vim.lsp.buf.clear_references()
-        vim.api.nvim_clear_autocmds { group = "kickstart-lsp-highlight", buffer = event2.buf }
+        vim.api.nvim_clear_autocmds { group = "user-lsp-highlight", buffer = event2.buf }
       end,
     })
   end
