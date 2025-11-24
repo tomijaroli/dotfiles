9f03c57a5bde3f6abd7d8d0e5389edf3d5ab6001
Tomi Jaroli <hi@tomijaroli.me>
2025-11-24 14:57:55 +0200

    [nvim] Remove bufferline plugin

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -5,14 +5,11 @@ local dependencies = {
   dependencies_registry.keys.web_devicons,
 }
 
-local bufferline = require "plugins.config.ui.bufferline"
 local lualine = require "plugins.config.ui.lualine"
 
-di.inject_dependencies_by_key(bufferline, dependencies, dependencies_registry)
 di.inject_dependencies_by_key(lualine, dependencies, dependencies_registry)
 
 return {
-  bufferline,
   lualine,
   require "plugins.config.ui.nightfox",
   require "plugins.config.ui.fidget",
